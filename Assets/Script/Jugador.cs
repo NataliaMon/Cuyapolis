4b22d362eb11e24bd690f61baf2f8cd5705e8e7e
Natalia Montoya <natalia.montoya.sanchez@gmail.com>
2017-11-01 19:29:15 -0500

    vidas

---
+++ Assets/Script/Jugador.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Jugador : MonoBehaviour {
+    public static int vidas = 5;
+    public static int oro;
+
+    public Text textoVidas;
+
+    void Update() {
+        textoVidas.text = vidas.ToString();
+
+        if (vidas <= 0) {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+}
--- Assets/Script/Meta.cs
+++ Assets/Script/Meta.cs
@@ -6,21 +6,12 @@ using UnityEngine.UI;
 
 public class Meta : MonoBehaviour {
 
-    public int ferret = 0;
-    public Text textoVidas;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("unidad"))
         {
-            ferret +=1;
-            textoVidas.text = (5 - ferret).ToString();
+            Jugador.vidas -= 1;
             GetComponent<AudioSource>().Play();
-            
-            if (ferret == 5)
-            {
-                SceneManager.LoadScene("GameOver");
-            }
         }
     }
 
